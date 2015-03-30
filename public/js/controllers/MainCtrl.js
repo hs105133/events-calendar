ec6130d43351f8e1a4145456a1bcb7adad1299cd
hs105133 <hemant.singh@techmahindra.com>
2015-03-30 10:22:41 +0530

    navbar collapse

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,6 @@
 angular.module("calApp")
     .controller('MainCtrl', function($scope, $location) {
+    	$scope.navbarCollapsed = true;
 		$scope.$path = $location.path.bind($location);        
     });
 
